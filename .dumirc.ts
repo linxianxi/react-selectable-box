1177f6c941c020b90c53cc74fc170ebbd8c33128
daisy <47104575+linxianxi@users.noreply.github.com>
2023-08-18 10:28:17 +0800

    feat: adjust box bg (#2)

--- .dumirc.ts
+++ .dumirc.ts
@@ -7,4 +7,5 @@ export default defineConfig({
     github: homepage,
   },
   html2sketch: {},
+  mfsu: false,
 });
--- .fatherrc.ts
+++ .fatherrc.ts
@@ -3,4 +3,5 @@ import { defineConfig } from 'father';
 export default defineConfig({
   cjs: { output: 'lib' },
   esm: { output: 'es' },
+  umd: { output: 'dist', name: 'reactSelectableBox' },
 });
