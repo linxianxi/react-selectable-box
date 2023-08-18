3a75155943b22dd38581f33a9d39423d218e898f
linxianxi <904492381@qq.com>
2023-08-18 13:37:32 +0800

    chore: adjust docs path

--- .dumirc.ts
+++ .dumirc.ts
@@ -1,11 +1,17 @@
 import { defineConfig } from 'dumi';
 import { homepage } from './package.json';
 
+const isProd = process.env.NODE_ENV === 'production';
+
+const name = 'react-selectable-box';
+
 export default defineConfig({
   themeConfig: {
-    name: 'react-selectable-box',
+    name,
     github: homepage,
   },
+  base: isProd ? `/${name}/` : '/',
+  publicPath: isProd ? `/${name}/` : '/',
   html2sketch: {},
   mfsu: false,
   outputPath: '.doc',
