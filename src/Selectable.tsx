5c04f59c983fd150baf09d39c65624fbc9121f52
daisy <47104575+linxianxi@users.noreply.github.com>
2023-09-12 14:46:36 +0800

    fix: add generics to SelectableProps (#15)

--- src/Selectable.tsx
+++ src/Selectable.tsx
@@ -1,20 +1,13 @@
-import React, {
-  forwardRef,
-  useEffect,
-  useImperativeHandle,
-  useMemo,
-  useRef,
-  useState,
-} from 'react';
+import React, { useEffect, useImperativeHandle, useMemo, useRef, useState } from 'react';
 import { createPortal } from 'react-dom';
 import { SelectableContext } from './context';
 import useEvent from './hooks/useEvent';
 import useLatest from './hooks/useLatest';
 import useMergedState from './hooks/useMergedState';
 
-interface SelectableProps {
-  defaultValue?: React.Key[];
-  value?: React.Key[];
+interface SelectableProps<T> {
+  defaultValue?: T[];
+  value?: T[];
   disabled?: boolean;
   children?: React.ReactNode;
   mode?: 'add' | 'remove' | 'reverse';
@@ -23,10 +16,7 @@ interface SelectableProps {
   boxStyle?: React.CSSProperties;
   boxClassName?: string;
   onStart?: () => void;
-  onEnd?: (
-    selectingValue: React.Key[],
-    changed: { added: React.Key[]; removed: React.Key[] },
-  ) => void;
+  onEnd?: (selectingValue: T[], changed: { added: T[]; removed: T[] }) => void;
 }
 
 export interface SelectableRef {
@@ -42,224 +32,224 @@ const getPortalContainer = (getContainer: () => HTMLElement = defaultGetContaine
   return null;
 };
 
-const Selectable = forwardRef<SelectableRef, SelectableProps>(
-  (
-    {
-      defaultValue,
-      value: propsValue,
-      disabled,
-      mode = 'add',
-      children,
-      selectStartRange = 'all',
-      getContainer,
-      boxStyle,
-      boxClassName,
-      onStart,
-      onEnd,
-    },
-    ref,
-  ) => {
-    const [isDragging, setIsDragging] = useState(false);
-    const [startCoords, setStartCoords] = useState({ x: 0, y: 0 });
-    const [moveCoords, setMoveCoords] = useState({ x: 0, y: 0 });
-    const selectingValue = useRef([]);
-    const [startTarget, setStartTarget] = useState<HTMLElement | null>(null);
-    const startInside = useRef(false);
-    const moveClient = useRef({ x: 0, y: 0 });
-    const [container, setContainer] = useState(() => getPortalContainer(getContainer));
-    const [value, setValue] = useMergedState(defaultValue || [], {
-      value: propsValue,
-    });
+function Selectable<T extends React.Key>(
+  {
+    defaultValue,
+    value: propsValue,
+    disabled,
+    mode = 'add',
+    children,
+    selectStartRange = 'all',
+    getContainer,
+    boxStyle,
+    boxClassName,
+    onStart,
+    onEnd,
+  }: SelectableProps<T>,
+  ref: React.ForwardedRef<SelectableRef>,
+) {
+  const [isDragging, setIsDragging] = useState(false);
+  const [startCoords, setStartCoords] = useState({ x: 0, y: 0 });
+  const [moveCoords, setMoveCoords] = useState({ x: 0, y: 0 });
+  const selectingValue = useRef<T[]>([]);
+  const [startTarget, setStartTarget] = useState<HTMLElement | null>(null);
+  const startInside = useRef(false);
+  const moveClient = useRef({ x: 0, y: 0 });
+  const [container, setContainer] = useState(() => getPortalContainer(getContainer));
+  const [value, setValue] = useMergedState(defaultValue || [], {
+    value: propsValue,
+  });
 
-    const startCoordsRef = useLatest(startCoords);
-    const isDraggingRef = useLatest(isDragging);
-    const selectStartRangeRef = useLatest(selectStartRange);
+  const startCoordsRef = useLatest(startCoords);
+  const isDraggingRef = useLatest(isDragging);
+  const selectStartRangeRef = useLatest(selectStartRange);
 
-    const top = Math.max(0, Math.min(startCoords.y, moveCoords.y));
-    const left = Math.max(0, Math.min(startCoords.x, moveCoords.x));
-    const width = isDragging ? Math.abs(startCoords.x - Math.max(0, moveCoords.x)) : 0;
-    const height = isDragging ? Math.abs(startCoords.y - Math.max(0, moveCoords.y)) : 0;
-    const boxRect = { top, left, width, height };
+  const top = Math.max(0, Math.min(startCoords.y, moveCoords.y));
+  const left = Math.max(0, Math.min(startCoords.x, moveCoords.x));
+  const width = isDragging ? Math.abs(startCoords.x - Math.max(0, moveCoords.x)) : 0;
+  const height = isDragging ? Math.abs(startCoords.y - Math.max(0, moveCoords.y)) : 0;
+  const boxRect = { top, left, width, height };
 
-    useEffect(() => {
-      setContainer(getPortalContainer(getContainer));
-    });
+  useEffect(() => {
+    setContainer(getPortalContainer(getContainer));
+  });
 
-    const checkScroll = () => {
-      if (isDraggingRef.current && container) {
-        const containerRect = container.getBoundingClientRect();
-        const x = moveClient.current.x - containerRect.left + container.scrollLeft;
-        const y = moveClient.current.y - containerRect.top + container.scrollTop;
+  const checkScroll = () => {
+    if (isDraggingRef.current && container) {
+      const containerRect = container.getBoundingClientRect();
+      const x = moveClient.current.x - containerRect.left + container.scrollLeft;
+      const y = moveClient.current.y - containerRect.top + container.scrollTop;
+      setMoveCoords({
+        x: Math.min(x, container.scrollWidth),
+        y: Math.min(y, container.scrollHeight),
+      });
+    }
+  };
+
+  useImperativeHandle(ref, () => ({
+    checkScroll,
+  }));
+
+  const handleStart = useEvent(() => {
+    if (!isDraggingRef.current) {
+      onStart?.();
+    }
+  });
+
+  const handleEnd = useEvent((newValue: T[]) => {
+    if (onEnd) {
+      const added: T[] = [];
+      const removed: T[] = [];
+
+      newValue.forEach((i) => {
+        if (value?.includes(i)) {
+          if (mode === 'remove' || mode === 'reverse') {
+            removed.push(i);
+          }
+        } else {
+          if (mode === 'add' || mode === 'reverse') {
+            added.push(i);
+          }
+        }
+      });
+      onEnd(newValue, { added, removed });
+    }
+  });
+
+  useEffect(() => {
+    let isMouseDowning = false;
+
+    const reset = () => {
+      setIsDragging(false);
+      setStartTarget(null);
+      isMouseDowning = false;
+      startInside.current = false;
+      selectingValue.current = [];
+    };
+
+    if (disabled || !container) {
+      reset();
+      return;
+    }
+
+    const onScroll = () => {
+      checkScroll();
+    };
+
+    const onMouseMove = (e: MouseEvent) => {
+      if (isMouseDowning) {
+        const { clientX, clientY } = e;
+        moveClient.current = { x: clientX, y: clientY };
+        const { left, top } = container.getBoundingClientRect();
+        const x = clientX - left + container.scrollLeft;
+        const y = clientY - top + container.scrollTop;
         setMoveCoords({
           x: Math.min(x, container.scrollWidth),
           y: Math.min(y, container.scrollHeight),
         });
+
+        if (!isDraggingRef.current) {
+          let shouldDraggingStart = true;
+          if (selectStartRangeRef.current === 'outside') {
+            shouldDraggingStart = !startInside.current;
+          } else if (selectStartRangeRef.current === 'inside') {
+            shouldDraggingStart = startInside.current;
+          }
+          const boxWidth = Math.abs(startCoordsRef.current.x - x);
+          const boxHeight = Math.abs(startCoordsRef.current.y - y);
+          // prevent trigger when click too fast
+          // https://github.com/linxianxi/react-selectable-box/issues/5
+          if (shouldDraggingStart && (boxWidth > 1 || boxHeight > 1)) {
+            setIsDragging(true);
+            handleStart();
+          }
+        }
       }
     };
 
-    useImperativeHandle(ref, () => ({
-      checkScroll,
-    }));
+    const onMouseUp = () => {
+      window.removeEventListener('mousemove', onMouseMove);
+      window.removeEventListener('mouseup', onMouseUp);
+      container.removeEventListener('scroll', onScroll);
 
-    const handleStart = useEvent(() => {
-      if (!isDraggingRef.current) {
-        onStart?.();
+      if (isDraggingRef.current) {
+        setValue(selectingValue.current);
+        handleEnd(selectingValue.current);
       }
-    });
+      reset();
+    };
 
-    const handleEnd = useEvent((newValue: React.Key[]) => {
-      if (onEnd) {
-        const added: React.Key[] = [];
-        const removed: React.Key[] = [];
+    const scrollListenerElement = container === document.body ? document : container;
 
-        newValue.forEach((i) => {
-          if (value?.includes(i)) {
-            if (mode === 'remove' || mode === 'reverse') {
-              removed.push(i);
-            }
-          } else {
-            if (mode === 'add' || mode === 'reverse') {
-              added.push(i);
-            }
-          }
-        });
-        onEnd(newValue, { added, removed });
-      }
-    });
+    const onMouseDown = (e: MouseEvent) => {
+      isMouseDowning = true;
 
-    useEffect(() => {
-      let isMouseDowning = false;
-
-      const reset = () => {
-        setIsDragging(false);
-        setStartTarget(null);
-        isMouseDowning = false;
-        startInside.current = false;
-        selectingValue.current = [];
-      };
-
-      if (disabled || !container) {
-        reset();
-        return;
+      if (selectStartRangeRef.current !== 'all') {
+        setStartTarget(e.target as HTMLElement);
       }
 
-      const onScroll = () => {
-        checkScroll();
-      };
+      const { clientX, clientY } = e;
+      const { left, top } = container.getBoundingClientRect();
+      setStartCoords({
+        x: clientX - left + container.scrollLeft,
+        y: clientY - top + container.scrollTop,
+      });
 
-      const onMouseMove = (e: MouseEvent) => {
-        if (isMouseDowning) {
-          const { clientX, clientY } = e;
-          moveClient.current = { x: clientX, y: clientY };
-          const { left, top } = container.getBoundingClientRect();
-          const x = clientX - left + container.scrollLeft;
-          const y = clientY - top + container.scrollTop;
-          setMoveCoords({
-            x: Math.min(x, container.scrollWidth),
-            y: Math.min(y, container.scrollHeight),
-          });
+      window.addEventListener('mousemove', onMouseMove);
+      window.addEventListener('mouseup', onMouseUp);
+      scrollListenerElement.addEventListener('scroll', onScroll);
+    };
 
-          if (!isDraggingRef.current) {
-            let shouldDraggingStart = true;
-            if (selectStartRangeRef.current === 'outside') {
-              shouldDraggingStart = !startInside.current;
-            } else if (selectStartRangeRef.current === 'inside') {
-              shouldDraggingStart = startInside.current;
-            }
-            const boxWidth = Math.abs(startCoordsRef.current.x - x);
-            const boxHeight = Math.abs(startCoordsRef.current.y - y);
-            // prevent trigger when click too fast
-            // https://github.com/linxianxi/react-selectable-box/issues/5
-            if (shouldDraggingStart && (boxWidth > 1 || boxHeight > 1)) {
-              setIsDragging(true);
-              handleStart();
-            }
-          }
-        }
-      };
+    container.addEventListener('mousedown', onMouseDown);
 
-      const onMouseUp = () => {
-        window.removeEventListener('mousemove', onMouseMove);
-        window.removeEventListener('mouseup', onMouseUp);
-        container.removeEventListener('scroll', onScroll);
+    return () => {
+      container.removeEventListener('mousedown', onMouseDown);
+      scrollListenerElement.removeEventListener('scroll', onScroll);
+      window.removeEventListener('mousemove', onMouseMove);
+      window.removeEventListener('mouseup', onMouseUp);
+    };
+  }, [disabled, container]);
 
-        if (isDraggingRef.current) {
-          setValue(selectingValue.current);
-          handleEnd(selectingValue.current);
-        }
-        reset();
-      };
+  const contextValue = useMemo(
+    () => ({
+      value,
+      selectingValue,
+      isDragging,
+      boxRect,
+      mode,
+      container,
+      startTarget,
+      startInside,
+    }),
+    [value, isDragging, top, left, width, height, mode, container, startTarget],
+  );
 
-      const scrollListenerElement = container === document.body ? document : container;
+  return (
+    <SelectableContext.Provider value={contextValue}>
+      {children}
+      {isDragging &&
+        container &&
+        createPortal(
+          <div
+            className={boxClassName}
+            style={{
+              position: 'absolute',
+              zIndex: 999,
+              top: 0,
+              left: 0,
+              transform: `translate(${left}px, ${top}px)`,
+              width,
+              height,
+              backgroundColor: 'rgba(22, 119, 255, 0.3)',
+              ...boxStyle,
+            }}
+          />,
+          container,
+        )}
+    </SelectableContext.Provider>
+  );
+}
 
-      const onMouseDown = (e: MouseEvent) => {
-        isMouseDowning = true;
-
-        if (selectStartRangeRef.current !== 'all') {
-          setStartTarget(e.target as HTMLElement);
-        }
-
-        const { clientX, clientY } = e;
-        const { left, top } = container.getBoundingClientRect();
-        setStartCoords({
-          x: clientX - left + container.scrollLeft,
-          y: clientY - top + container.scrollTop,
-        });
-
-        window.addEventListener('mousemove', onMouseMove);
-        window.addEventListener('mouseup', onMouseUp);
-        scrollListenerElement.addEventListener('scroll', onScroll);
-      };
-
-      container.addEventListener('mousedown', onMouseDown);
-
-      return () => {
-        container.removeEventListener('mousedown', onMouseDown);
-        scrollListenerElement.removeEventListener('scroll', onScroll);
-        window.removeEventListener('mousemove', onMouseMove);
-        window.removeEventListener('mouseup', onMouseUp);
-      };
-    }, [disabled, container]);
-
-    const contextValue = useMemo(
-      () => ({
-        value,
-        selectingValue,
-        isDragging,
-        boxRect,
-        mode,
-        container,
-        startTarget,
-        startInside,
-      }),
-      [value, isDragging, top, left, width, height, mode, container, startTarget],
-    );
-
-    return (
-      <SelectableContext.Provider value={contextValue}>
-        {children}
-        {isDragging &&
-          container &&
-          createPortal(
-            <div
-              className={boxClassName}
-              style={{
-                position: 'absolute',
-                zIndex: 999,
-                top: 0,
-                left: 0,
-                transform: `translate(${left}px, ${top}px)`,
-                width,
-                height,
-                backgroundColor: 'rgba(22, 119, 255, 0.3)',
-                ...boxStyle,
-              }}
-            />,
-            container,
-          )}
-      </SelectableContext.Provider>
-    );
-  },
-);
-
-export default Selectable;
+export default React.forwardRef(Selectable) as <T extends React.Key>(
+  props: SelectableProps<T> & React.RefAttributes<SelectableRef>,
+) => React.ReactElement;
